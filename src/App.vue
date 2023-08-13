b70918fe8bb276e2de650b92f0b3f9472dacf273
jermp96 <jjeremyperez96@gmail.com>
2023-08-13 15:28:51 -0600

    init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  </header>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  
+})
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+</style>
