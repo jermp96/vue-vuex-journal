4c93c0b5bd4aa0105b0dbe04778d0ccaa51754f5
jermp96 <jjeremyperez96@gmail.com>
2023-08-26 11:50:39 -0600

    feat: EntryList and Entry components were added

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2">
+    <!--Entry titulo-->
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">26</span>
+      <span class="mx-1 fs-5">Agosto</span>
+      <span class="mx-2 fw-light">2023, Sábado </span>
+    </div>
+
+    <div class="entry-description">
+      Fugiat consectetur commodo duis quis nisi velit pariatur id occaecat ullamco dolor. Aliquip ullamco nostrud consequat voluptate anim velit officia ut sint commodo ut irure velit minim. Laborum culpa consequat reprehenderit nisi deserunt do irure enim irure anim. Dolore enim aliquip ex dolor aute in qui laborum. Fugiat ea pariatur ipsum voluptate amet occaecat labore velit ipsum elit proident velit amet. Nostrud duis aute aute dolor eu incididunt culpa voluptate. Culpa ad irure fugiat mollit nisi esse pariatur consequat elit proident sint.
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  
+})
+</script>
+
+<style lang="scss">
+  .entry-container{
+    border-bottom: 1px solid #2ce350;
+    transition: 0.2s all ease-in;
+    &:hover{
+      background-color: lighten($color: grey, $amount: 45);
+      transition: 0.2s all ease-in;
+    }
+
+    .entry-description{
+      font-size: 12px;
+    }
+  }
+</style>
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    EntryList
+  <div class="entry-list-container">
+   <div class="px-2 pt-2">
+    <input type="text" class="form-control" placeholder="buscar entrada">
+   </div>
+   <div class="entry-scrollarea">
+     <Entry v-for="item in 100" :key="item"/>
+   </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineAsyncComponent, defineComponent } from 'vue';
 
 export default defineComponent({
-  
+  components: {
+    Entry: defineAsyncComponent(() => import('./Entry.vue'))
+  }
 })
 </script>
+
+<style lang="scss" scoped>
+  .entry-list-container{
+    border-right: 1px solid #2c3e50;
+    height: calc(100vh - 56px);
+  }
+
+  .entry-scrollarea{
+    height: calc(100vh - 120px);
+    overflow-y: scroll;
+  }
+</style>
